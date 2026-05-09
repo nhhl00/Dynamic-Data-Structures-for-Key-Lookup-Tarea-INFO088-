5f3c9112cb77b264b69fe3d69005e5d545e1a9c9
nhhl00 <agustin8montenegro@gmail.com>
2026-05-09 17:02:08 -0400

    struct vector y construccion

--- solucion1.cpp
+++ solucion1.cpp
@@ -2,57 +2,58 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <ctime>
 
 using namespace std;
 
-#define OVERHEAD 50
+#define OVERHEAD 20
 #define CAPACIDAD_INICIAL 4
-#define SIGMA 26
+#define SIGMA 256
+#define REPET 10
 
-void construir_vector(string archivo);
-void eliminar(vector<string>& v);
-void insertar(vector<string>& v);
-void buscar(vector<string>& v);
+//struct de vector uchar*
+struct Solucion1{
+    vector<unsigned char*> v;
+    int inicio[SIGMA];
+    int capacidad;
+    int size_v;
 
-//variables
-vector<string> v;
-int size_v = 0;
-int capacidad = CAPACIDAD_INICIAL;
-int overhead = OVERHEAD;
-int inicio[SIGMA];
+    Solucion1(){
+        capacidad = CAPACIDAD_INICIAL;
+        size_v = 0;
+        for (int i=0; i<SIGMA;i++)inicio[i] = -1;
+    }
+};
+
+void construir_vector(Solucion1& sol, string archivo);
 
 int main(){
-    construir_vector("D1.txt");
-    for (int i=0; i < size_v-1; i++){
-        cout << v[i] << endl;
+    Solucion1 sol;
+    construir_vector(sol, "D1.txt");
+    cout << "empieza programa..." << endl;
+    for (int i=0; i<sol.size_v; i++){
+        cout << sol.v[i] << endl;
     }
+    return 0;
 }
 
-void construir_vector(string archivo){
+void construir_vector(Solucion1& sol, string archivo){
     ifstream d1(archivo);
     if (!d1){
         cout << "archivo de texto " << archivo << "no encontrado" << endl;
         return;
     }
-    //llenar vector "v" con los elementos uchar de D1.txt
+    //leer D1 y llenar vector "v" con uchar*
     string linea;
     while (getline(d1, linea)){
         if (!linea.empty()) {
-            v.push_back(linea);
-            size_v++;
-        }
+            unsigned char* palabra = new unsigned char [linea.size() + 1];
+            for (int i = 0; i<= (int)linea.size(); i++){
+                palabra[i] = (unsigned char)linea[i];
+            }
+            sol.v.push_back(palabra);
+            sol.size_v++;
+            }
     }
     d1.close();
-
-    //ordenar vector "v" de strings
-    for (int i=0; i < size_v-1; i++){
-        for (int j=0; j < size_v-i-1; j++){
-            if (v[j] > v[j+1]){
-                string temporal = v[j];
-                v[j] = v[j+1];
-                v[j+1] = temporal;
-            }
-        }
-    }
-    capacidad = size_v;
 }
